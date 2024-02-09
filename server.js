700fece1ad0e1e5be2d0368baa6d6a45059cf2e1
Chris DeJong <magiktrikdev@gmail.com>
2024-02-09 11:56:00 -0600

    Backend Stuff

--- server.js
+++ server.js
@@ -7,18 +7,59 @@
 // the user. Express gives us a way to expose a *public* API that users can see.
 //
 
-import { handler } from './build/handler.js';
-import express from 'express';
-import cors from 'cors';
+import express      from 'express';
+import cors         from 'cors';
+import { handler }  from './build/handler.js';
+import dbman        from './server/databases.js';
 
-// Obligatory joke goes here.
-const port = 9001;
-const app = express();
+const test_database = null;
+const port          = 9001;
+const app           = express();
 
-// Async API definitions.
+// --- CORS --------------------------------------------------------------------
+
 app.use(cors()); // Required for cross-origin resource sharing support.
-app.get('/data', (request, response) => {
-    response.json({ "hello": "world" });
+
+// --- Async API Definitions ---------------------------------------------------
+
+app.get('/api/:endpoint', (request, response) => {
+
+    // API endpoint definition.
+    response.json({ "endpoint": `${ request.params.endpoint }` });
+
+});
+
+// --- Database Modification API -----------------------------------------------
+//
+// For doing that wack-type shit.
+//
+
+app.get('/database/:command/:on', (request, response) => {
+
+    // We can pull the command from the params object.
+    let command_type    = request.params.command;
+    let command_sub     = request.params.on;
+
+    // In case we don't know what we're doing, here is nifty little guide.
+    if (command_type == "help")
+    {
+        let help_dialogue = "";
+        help_dialogue += `<p><strong>Database Rebuild</strong> /database/rebuild/db_name</p>`;
+        help_dialogue += `<p><i>Reconstructs the databases back to the defaults.</i></p>`;
+
+        help_dialogue += `<p><strong>Database Rebuild</strong> /database/rebuild/db_name</p>`;
+        help_dialogue += `<p><i>Reconstructs the databases back to the defaults.</i></p>`;
+    }
+
+    // If the command is a reset command, then we can process it here.
+    if (command_type == "reset")
+    {
+        
+        let dbi = dbman.database_utils.find(command_sub);
+        if (dbi != null) dbi.database_reset();
+
+    }
+
 });
 
 // Basically hands off the server-side routing back SvelteKit.
---
+++ src/routes/aboutus/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>About Us</title>
+	<meta name="description" content="About Us" />
+</svelte:head>
+
+<section>
+    <h1>This is the about page.</h1>
+    <a href="/">Return to index.</a>
+</section>
